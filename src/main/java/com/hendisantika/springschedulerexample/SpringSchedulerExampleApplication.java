5d799d838b14bce609ac9cc93e3ef543e0f923f9
Hendi Santika <hendisantika@gmail.com>
2018-08-02 07:53:49 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springschedulerexample/SpringSchedulerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springschedulerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSchedulerExampleApplication.class, args);
+    }
+}
