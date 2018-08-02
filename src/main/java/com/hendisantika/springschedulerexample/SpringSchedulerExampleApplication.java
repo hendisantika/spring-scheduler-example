fbe05a1347ae8b887d8600cea8798d878406df06
Hendi Santika <hendisantika@gmail.com>
2018-08-02 07:55:08 +0700

    #3 - Modify Main app

--- src/main/java/com/hendisantika/springschedulerexample/SpringSchedulerExampleApplication.java
+++ src/main/java/com/hendisantika/springschedulerexample/SpringSchedulerExampleApplication.java
@@ -1,12 +1,24 @@
 package com.hendisantika.springschedulerexample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringSchedulerExampleApplication {
+
+    private Logger LOG = LoggerFactory.getLogger(SpringSchedulerExampleApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSchedulerExampleApplication.class, args);
+    }
+
+    @Scheduled(initialDelay = 3000, fixedDelay = 2000)
+    public void takeAction() {
+        LOG.info("Running the @Scheduled task...");
     }
 }
